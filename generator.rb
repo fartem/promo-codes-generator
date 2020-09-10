8c6c88553adeae6ee165358c1170c92452da6ec7
Artem Fomchenkov <45129887+fartem@users.noreply.github.com>
2020-09-10 07:08:33 +0300

    2020-09-10 Update: Updated error message text

--- generator.rb
+++ generator.rb
@@ -5,7 +5,7 @@ prefix = ARGV[1]
 count = ARGV[2]
 
 if length.nil? || prefix.nil? || count.nil?
-  puts 'Uncorrect format. Usage: ruby generator.rb [addition length] [prefix] [promos count]'
+  puts 'Uncorrect format. Usage: ruby generator.rb [length] [prefix] [count]'
 else
   characters = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
 
